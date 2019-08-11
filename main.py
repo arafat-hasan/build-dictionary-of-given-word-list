dcf938c06347daaea0d83bbe7c7f12ad6072e01f
Arafat Hasan Jenin <opendoor.arafat@gmail.com>
2019-08-11 22:40:08 +0600

    Latex done, needs to work more

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ from requests.exceptions import Timeout
 from pylatex import Document, Section, Subsection, Command, UnsafeCommand, \
         PageStyle, Head, MiniPage, Foot, LargeText, MediumText, LineBreak, \
         simple_page_number
-from pylatex.utils import italic, NoEscape, bold
+from pylatex.utils import italic, NoEscape, bold, escape_latex
 from pylatex.package import Package
 from pylatex.base_classes import Environment, CommandBase, Arguments
 
@@ -101,14 +101,35 @@ def processWord(eng, ben, prep, example):
     return 0
 
 def processLaTex():
+
+    titlePage = r'''
+\newcommand*{\rotrt}[1]{\rotatebox{90}{#1}} % Command to rotate right 90 degrees
+\newcommand*{\rotlft}[1]{\rotatebox{-90}{#1}} % Command to rotate left 90 degrees
+
+\title{\
+	\def\CP{\textit{\Huge The Dictionary of Personal Words}} \	
+	\settowidth{\unitlength}{\CP} \
+	{\color{LightGoldenrod}\resizebox*{\unitlength}{\baselineskip}{\rotrt{$\}$}}} \\[\baselineskip]\
+	\textcolor{Sienna}{\CP} \\[\baselineskip] \
+	{\color{RosyBrown}\Large AN ILLUSTRATED COLLECTION} \\ \
+	{\color{LightGoldenrod}\resizebox*{\unitlength}{\baselineskip}{\rotlft{$\}$}}} \
+	}
+
+\author{\
+	\Large\textbf{Arafat Hasan} \
+	}
+
+\date{}
+'''
+
+
     geometry_options = {"top":"2.3cm","bottom":"2.0cm", "left":"2.5cm",\
-            "right":"2.0cm", "columnsep":"20pt"}
+            "right":"2.0cm", "columnsep":"27pt"}
     doc = Document('basic', geometry_options=geometry_options)
-    first_page = PageStyle("firstpage")
     doc.documentclass = Command(
             'documentclass',
             options=['10pt', 'a4paper', 'twoside'],
-            arguments=['article'],
+            arguments=['book'],
             )
 
     doc.packages.append(Package('palatino'))
@@ -116,8 +137,14 @@ def processLaTex():
     doc.packages.append(Package('multicol'))
     doc.packages.append(Package('fontspec'))
     doc.packages.append(Package('enumitem'))
+    doc.packages.append(Package('graphicx'))
+    doc.packages.append(Package('PTSerif'))
     doc.packages.append(Package('titlesec', NoEscape('bf, sf, center')))
     doc.packages.append(Package('fancyhdr'))
+    
+
+    doc.preamble.append(Command('usepackage', \
+            NoEscape(r'xcolor'), 'svgnames'))
 
 
 
@@ -138,16 +165,29 @@ def processLaTex():
 
     doc.preamble.append(Command('pagestyle', 'fancy'))
 
-    new_comm = UnsafeCommand('newcommand', '\entry', options=4, \
-    # extra_arguments=NoEscape(r'\textbf{#1}\markboth{#1}{#1}\ {(#2)}\ \textit{#3}\  \ {#4}'))
-        extra_arguments=NoEscape(r'\textbf{#1}\markboth{#1}{#1}\ {{\fontspec{Doulos SIL} #2}}\  {{\fontspec{Kalpurush} #3}}\ $\bullet$\ {#4}'))
+    doc.append(NoEscape(r'\setlength{\parindent}{-0.7em}')) 
+
+    new_comm = UnsafeCommand('newcommand', '\entry', options=7, \
+    extra_arguments=NoEscape(r'\textbf{#1}\markboth{#1}{#1}\ {{\fontspec{Doulos SIL} #2}}\  {{\fontspec{Kalpurush} #3}}\ {#4}\ {#5}\ {\textit{#6}}\ {#7}'))
     doc.append(new_comm)
 
+    color_bullet = UnsafeCommand('newcommand', '\colorBulletS', options=1, \
+    extra_arguments=NoEscape(r'\colorbox[RGB]{171,171,171}{\makebox(11,2){\textcolor{white}{{\tiny \textbf{#1}}}}}'))
+    doc.append(color_bullet)
 
-    # print(type(fileData))
+    color_bullet = UnsafeCommand('newcommand', '\colorBullet', options=1, \
+    extra_arguments=NoEscape(r'\colorbox[RGB]{171,171,171}{\makebox(22, 1){\textcolor{white}{{\tiny \textbf{#1}}}}}'))
+    doc.append(color_bullet)
+
+
+    doc.preamble.append(NoEscape(titlePage))
+    doc.append(NoEscape(r'\maketitle'))
     entriesList = list(fileData.keys())
     entriesList.sort()
 
+    currentSection = "a"
+    sectionStr = "\section*{" + currentSection.upper() + "}"
+    doc.append(NoEscape(sectionStr))
     doc.append(NoEscape(r'\begin{multicols}{2}'))
 
     for item in entriesList:
@@ -161,43 +201,66 @@ def processLaTex():
            phonetic     = entry.get('phonetic',"")
            meaning      = entry.get('meaning',{})
 
+
+           word         = escape_latex(word)
+           bengali      = escape_latex(bengali)
+           prep         = escape_latex(prep)   
+           ownExample   = escape_latex(ownExample)
+           origin       = escape_latex(origin)
+           phonetic     = escape_latex(phonetic)
+           
+           if len(prep): prep = " \\colorBullet{OTHER} " + prep
+           if len(origin): origin = " \\colorBullet{ORIGIN} " + origin
+
            partsOfSpeech = list(meaning.keys())
 
-           partStr = ""
+           if len(partsOfSpeech) == 1:
+               partStr = ""
+           else:
+               partStr = "\\small{\\textsf{\\textit{" + escape_latex(", ".join(partsOfSpeech)) + "}}}"
            
            for part in partsOfSpeech:
+               escapePart = escape_latex(str(part))
                onepart     = meaning[part]
                deffCnt = 0
-               strGen = "\\textsf{" + part + "}"
+               if len(partsOfSpeech) == 1:
+                   strGen = "\\textsf{\\textit{" + escapePart + "}}\\"
+               else:
+                   strGen = "\\\\{\\fontspec{DejaVu Sans}▪ }\\textsf{\\textit{" + escapePart + "}}\\\\"
+
                for deff in onepart:
                    deffCnt = deffCnt + 1
                    definition  = deff.get('definition',"")
                    example     = deff.get('example',"")
-                   synonyms    = deff.get('synonyms',"")
-                   # print(word, part, definition)
-                   # print(u', '.join((word, phonetic, part, definition)).encode('utf-8').strip())
-                   # print("#############################")
-                   # print(u''.join((lorem)).encode('utf-8').strip())
-                   # doc.append(u', '.join((word, phonetic, part, definition)).encode('utf-8').strip())
-                   # lorem.replace("&", "\&")
-                   strGen = strGen + " \\textbf{" + str(deffCnt) + "} " + definition + " {\\fontspec{Times New Roman}◇} " + "\\textit{" + example + "}"
+                   synonyms    = deff.get('synonyms',{})
+
+                   definition   = escape_latex(definition)
+                   example      = escape_latex(example)
+                   synonyms     = escape_latex(", ".join(synonyms))
+                   if len(synonyms): synonyms     = " \\colorBulletS{SYN} " + synonyms
+
+                   strGen = strGen + " \\textbf{" + str(deffCnt) + "} " + definition + " {\\fontspec{DejaVu Sans}◇} " + "\\textit{" + example + "}" + synonyms
 
                partStr = partStr + " " + strGen
 
-           lorem = "\entry{"+word+"}{"+phonetic+"}{"+bengali+"}{"+partStr+"}"
+           lorem = "\entry{"+word+"}{"+phonetic+"}{"+bengali+"}{"+partStr+"}" + "{" + prep +"}" + "{" + ownExample + "}" + "{" + origin + "}"
+           if item[0] is not currentSection[0]:
+               currentSection = item[0]
+               sectionStr = "\section*{" + currentSection.upper() + "}"
+               doc.append(NoEscape(r'\end{multicols}'))
+               doc.append(NoEscape(r'\pagebreak'))
+               doc.append(NoEscape(sectionStr))
+               doc.append(NoEscape(r'\begin{multicols}{2}'))
            doc.append(NoEscape(lorem))
            doc.append(NoEscape(r'\par'))
                
 
 
-    print("******************************************************")
-    # doc.append(NoEscape(r'\section*{A}'))
-    # doc.append(NoEscape(r'test'))
     doc.append(NoEscape(r'\end{multicols}'))
     doc.generate_pdf(clean_tex=False, compiler='xelatex')
     doc.generate_tex()
 
-    tex = doc.dumps()  # The from pylatex.utils import italic, NoEscapedocument as string in LaTeX syntax
+    tex = doc.dumps() 
 
 
 
@@ -205,34 +268,34 @@ def processLaTex():
 
 def main():
 
-    # with open('words.csv') as csvFile:
-    #     csvReader = csv.reader(csvFile, delimiter=',')
-    #     lineCount = 0
-    #     unmanagedWord = 0
-    #     for row in csvReader:
-    #         if lineCount == 0:
-    #             lineCount += 1
-    #         else:
-    #             lineCount += 1
-    #             if len(row) < 1:
-    #                 continue
-    #             ret = processWord(row[0], row[1], row[2], row[3])
-    #             if ret == 100:
-    #                 print("Request timed out")
-    #                 print("Check internet connection")
-    #                 break
-    #             elif ret == 4:
-    #                 unmanagedWord = unmanagedWord + 1
-    #         sys.stdout.write("\r%d lines processed so far..." % lineCount)
-    #         sys.stdout.flush()
-    #     print(f'\nProcessed {lineCount} lines total, %d word(s) not processed\n'
-    #             % unmanagedWord)
+    with open('words.csv') as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=',')
+        lineCount = 0
+        unmanagedWord = 0
+        for row in csvReader:
+            if lineCount == 0:
+                lineCount += 1
+            else:
+                lineCount += 1
+                if len(row) < 1:
+                    continue
+                ret = processWord(row[0], row[1], row[2], row[3])
+                if ret == 100:
+                    print("Request timed out")
+                    print("Check internet connection")
+                    break
+                elif ret == 4:
+                    unmanagedWord = unmanagedWord + 1
+            sys.stdout.write("\r%d lines processed so far..." % lineCount)
+            sys.stdout.flush()
+        print(f'\nProcessed {lineCount} lines total, %d word(s) not processed\n'
+                % unmanagedWord)
 
 
-    #     with io.open("dic.json", mode='w+', encoding='utf-8') as jsonFile:
-    #         json.dump(fileData, jsonFile, ensure_ascii=False, indent=4, sort_keys=True)
-    #     jsonFile.close()
-    # csvFile.close()
+        with io.open("dic.json", mode='w+', encoding='utf-8') as jsonFile:
+            json.dump(fileData, jsonFile, ensure_ascii=False, indent=4, sort_keys=True)
+        jsonFile.close()
+    csvFile.close()
     processLaTex()
 
     print("Process Completed\n")
